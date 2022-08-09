a92a75e6f30835f36179d9044ebd1abcb2063198
Nicholas Corgan <n.corgan@gmail.com>
2022-08-08 20:40:19 -0500

    Some stream API

--- mex/common/m/SoapySDR_Stream.m
+++ mex/common/m/SoapySDR_Stream.m
@@ -33,4 +33,47 @@ classdef SoapySDR_Stream < handle
         %DELETE Destructor.
             % TODO
         end
+
+        %
+        % Operations
+        %
+
+        function close(this)
+        %CLOSE Close the stream.
+            SoapySDR_MEX("Stream_close", this.__internal);
+        end
+
+        function activate(this, flags, timeNs, numElems)
+        %ACTIVATE Begin streaming.
+            flags_ = 0;
+            timeNs_ = 0;
+            numElems_ = 0;
+
+            if nargin >= 2
+                flags_ = flags;
+            end
+            if nargin >= 2
+                timeNs_ = timeNs;
+            end
+            if nargin >= 2
+                numElems_ = numElems;
+            end
+
+            SoapySDR_MEX("Stream_activate", this.__internal, flags_, timeNs_, numElems_);
+        end
+
+        function deactivate(this, flags, timeNs)
+        %DEACTIVATE Stop streaming.
+            flags_ = 0
+            timeNs_ = 0
+
+            if nargin >= 2
+                flags_ = flags;
+            end
+            if nargin >= 2
+                timeNs_ = timeNs;
+            end
+
+            SoapySDR_MEX("Stream_deactivate", this.__internal, flags_, timeNs_);
+        end
 end
